c88eb64dd9f504ef99de4f64e0fea477b07c47e3
MustinJoss <31445480+MustinJoss@users.noreply.github.com>
2018-02-22 18:53:03 -0600

    Clean up, CSS
    Cleaned up dice logic and moved CSS elements to .css file.

--- script.js
+++ script.js
@@ -6,6 +6,10 @@ var d20 = {
     sides: 20,
 }
 
+var d12 = {
+    sides: 12,
+}
+
 var d10 = {
     sides: 10,
 }
@@ -25,24 +29,19 @@ var d4 = {
 var diceType = {
     "d100" : d100,
     "d20" : d20,
+    "d12" : d12,
     "d10" : d10,
     "d8" : d8,
     "d6" : d6,
     "d4" : d4,
 }
-
+// diceCount to enable addition of multiple dice roles - later implemntation.
 function rollDice(diceType, diceCount){
-    let display = document.getElementById("status");
-    let result = Math.floor(Math.random() * diceType.sides) + 1;
-    display.innerHTML = "You rolled " + result + " on a d" + diceType.sides + ".";
-}
-
-function totalUp(diceType, diceCount){
     let total = 0;
     let display = document.getElementById("status");
     for (i=0; i<diceCount; i++) {
         let roll = Math.floor(Math.random() * diceType.sides) + 1;
         total += roll;
     }
-    display.innerHTML = "You rolled " + total + " on " + diceCount + "d" + diceType.sides + ".";
+        display.innerHTML = "You rolled " + total + " on " + diceCount + "d" + diceType.sides + ".";
 }
